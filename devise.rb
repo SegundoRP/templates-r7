2c5f2cd3780ae6eed46a400e09a811fc7be9cd56
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-04-19 00:28:18 -0500

    feat: :fire: comment readme declaration

--- devise.rb
+++ devise.rb
@@ -72,10 +72,10 @@ end
 
 # README
 ########################################
-markdown_file_content = <<~MARKDOWN
-  Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
-MARKDOWN
-file "README.md", markdown_file_content, force: true
+# markdown_file_content = <<~MARKDOWN
+#   Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+# MARKDOWN
+# file "README.md", markdown_file_content, force: true
 
 # Generators
 ########################################
